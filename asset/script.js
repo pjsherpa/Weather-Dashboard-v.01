0b93c14598b81caa812642259d19565e2f5e5648
Phinjock Sherpa <phinjock@gmail.com>
2022-07-24 20:26:50 -0700

    fixed local storage and WIP for addeventListene for storedSearches

--- asset/script.js
+++ asset/script.js
@@ -1,61 +1,77 @@
-// create DOM so I can access my codes to the display section
-// form-write a city's name and a local storage is waiting for this to store it
 var searchforCity = document.getElementById("searchforCity");
-
-// for addEventListener the local storage is activated once this addvenlistener is activated
 var searchbtn = document.getElementsByClassName("btn");
-// display new-btn here:We need to have an event target activated here so we can use this again
 var listBtn = document.getElementById("listBtn");
-//display current weather here
 var currentWeather = document.getElementById("weatherContainer");
-//display weather forecast here
 var forecast = document.getElementById("Forecast");
-//api Key to be added when we fetch the url
 var apiKey = "f2c131fc5bc12a5320fc9c5062b3a515";
-var selectCity;
+var previousSearch = [];
 
-renderLastCityName();
+function renderSearch() {
+  // Clear todoList element and update todoCountSpan
+  var cityName = searchforCity.value.trim();
+
+  // Render a new li for each todo
+  for (var i = 0; i < previousSearch.length; i++) {
+    var previousSearches = previousSearch[i];
+
+    var li = document.createElement("li");
+    li.classList = "btn btn-secondary data-city";
+    li.style.marginTop = "30px";
+    li.style.width = "18rem";
+    li.textContent = previousSearches;
+    li.value.trim = previousSearches;
+    console.log(li);
+    li.setAttribute("data-index", i);
+
+    var newBtn = document.createElement("button");
+
+    newBtn.appendChild(li);
+    listBtn.appendChild(li);
+  }
+}
+// This function is being called below and will run when the page loads.
+function init() {
+  // Get stored todos from localStorage
+  var storedSearches = JSON.parse(localStorage.getItem("previousSearch"));
+  // If todos were retrieved from localStorage, update the todos array to it
+  if (storedSearches !== null) {
+    previousSearch = storedSearches;
+  }
+  // This is a helper function that will render todos to the DOM
+  renderSearch();
+}
+
+function storeSearches() {
+  // Stringify and set key in localStorage to todos array
+  localStorage.setItem("previousSearch", JSON.stringify(previousSearch));
+}
+
 var handleFormSubmit = function (event) {
   event.preventDefault();
 
   var cityName = searchforCity.value.trim();
-  localStorage.setItem("cityName", JSON.stringify(cityName));
   if (cityName) {
-    var printCityname = function (City) {
-      getCitySearch(City);
-      var newBtn = document.createElement("button");
-      newBtn.classList = "btn btn-secondary data-city";
-      newBtn.style.marginTop = "30px";
-      newBtn.style.width = "18rem";
-      var listDetail = City;
-      newBtn.textContent = listDetail;
-      listBtn.appendChild(newBtn);
-      // reset form
-      searchforCity.value.trim("");
-    };
+    getCitySearch(cityName);
+    previousSearch.push(cityName);
+    // searchforCity.value = "";
   } else {
     alert("Please enter a City name");
   }
-  printCityname(cityName);
+  // renderSearch();
+  storeSearches();
 };
 
 var buttonClickHandler = function (event) {
   event.preventDefault();
-  //the name of city choices given on the beginning
-  selectCity = event.target.getAttribute("btn-secondary");
-
+  var selectCity = event.target;
+  console.log(selectCity);
   if (selectCity) {
-    var lastCityName = getCitySearch(selectCity);
-
-    //haven't linked this yet or created on HTML
+    getCitySearch(selectCity);
   }
-  console.log("button clicked", lastCityName);
 };
-//create the buttons
 function getCitySearch(search) {
   var url = `https://api.openweathermap.org/data/2.5/weather?q=${search}&units=imperial&appid=${apiKey}`;
-  //this clears out the weathercotainer when multipleclicks are made
-
+  //this clears out the weathercontainer when multipleclicks are made
   currentWeather.textContent = " ";
   forecast.textContent = " ";
   console.log(url);
@@ -74,18 +90,17 @@ function getCitySearch(search) {
           return response.json();
         })
         .then(function (data, search) {
+          // current weather Display
           var hCity = document.createElement("h3");
           var city = searchforCity.value.trim();
           hCity.textContent = city;
-
           var htoday = document.createElement("h3");
+          // converted dt to date
           var dtCon = data.daily[0].dt;
           var milliseconds = dtCon * 1000;
           var date = new Date(milliseconds);
           var humanDateFormattoday = date.toLocaleDateString("en-us");
-
           htoday.textContent = humanDateFormattoday;
-
           var ptemp = document.createElement("p");
           var temp = `Temp:${data.current.temp} ℉`;
           ptemp.textContent = temp;
@@ -118,6 +133,7 @@ function getCitySearch(search) {
             //severe
             sUvi.style.background = "red";
           }
+          // 5 day weather foreacast display
           for (i = 1; i < 6; i++) {
             var div = document.createElement("div");
             div.classList.add("col-sm-2");
@@ -125,13 +141,11 @@ function getCitySearch(search) {
             div2.classList.add("card");
             var div3 = document.createElement("div");
             div3.classList.add("card-body");
-
             var imgEl = document.createElement("img");
             var icon = data.daily[i].weather[0].icon;
             var iconurl = "http://openweathermap.org/img/wn/" + icon + ".png";
             imgEl.setAttribute("src", iconurl);
             console.log(iconurl);
-
             var pTemps = document.createElement("p");
             var temps = `Temp:${data.daily[i].temp} ℉`;
             pTemps.textContent = temp;
@@ -141,16 +155,14 @@ function getCitySearch(search) {
             var phumiditys = document.createElement("p");
             var humiditys = `Humidity:${data.daily[i].humidity}`;
             phumiditys.textContent = humiditys;
-            //ref https://coderrocketfuel.com/article/convert-a-unix-timestamp-to-a-date-in-vanilla-javascript
+            //ref https://coderrocketfuel.com/article/convert-a-unix-timestamp-to-a-date-in-vanilla-javascript converting dt to date
             var dtConvert = data.daily[i].dt;
             var milliseconds = dtConvert * 1000;
             var dates = new Date(milliseconds);
             var humanDateFormat = dates.toLocaleDateString("en-us");
-
             var pdates = document.createElement("p");
             console.log(humanDateFormat);
             pdates.textContent = humanDateFormat;
-
             div3.appendChild(pdates);
             div3.appendChild(imgEl);
             div3.appendChild(pTemps);
@@ -163,11 +175,8 @@ function getCitySearch(search) {
         });
     });
 }
-function renderLastCityName(event) {
-  lastCityName = localStorage.getItem("cityName");
-  searchforCity.value.trim(listBtn);
-}
 
 searchbtn[0].addEventListener("click", handleFormSubmit);
-renderLastCityName();
 listBtn.addEventListener("click", buttonClickHandler);
+
+init();
